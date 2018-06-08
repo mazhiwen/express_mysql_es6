c2a4e942f5689f0ad9613fcee012ba979f070ad6
marjoven <81245995@qq.com>
2018-06-08 10:48:20 +0800

    mysql数据库查询测试

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database:'mysql',
+  insecureAuth : true
+});
+connection.connect();
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  
+
+  
+
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    // console.log(err);
+    // console.log(rows);
+    // console.log(fields);
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+    res.send(rows);
+  });
+  connection.end();
+
+
+
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
