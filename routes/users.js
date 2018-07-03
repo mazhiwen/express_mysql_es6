442a4f6c40a21a223d64b23efdeab5927d9cca83
marjoven <81245995@qq.com>
2018-07-03 17:53:48 +0800

    登陆处理

--- routes/users.js
+++ routes/users.js
@@ -40,21 +40,40 @@ router.post('/create', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  mysql.query(`INSERT INTO users (accout,password) values (${req.body.account},${req.body.password})`, function(err, rows, fields) {
+  mysql.query(`select password from users where accout='${req.body.account}'`, function(err, rows, fields) {
+    
     if (err) {
-      let  message='错误';
-      if(err.code=='ER_DUP_ENTRY'){
-        message='用户名称重复'
-      }
+      let  message='系统错误';
       res.send({
         status:500,
         message:message
       });
     }else{
-      res.send({
-        status:200,
-        message:'成功'
-      });
+      
+      if(rows.length>0){
+        if(rows[0].password==req.body.password){
+          let token=jwt.sign({account:req.body.account},JWTSecret);
+          res.send({
+            status:200,
+            message:'成功',
+            data:{
+              token:token
+            }
+          });
+        }else{
+          res.send({
+            status:400,
+            message:'密码错误'
+          });
+        }
+      }else{
+        res.send({
+          status:400,
+          message:'账号不存在'
+        });
+      }
+      
+      
     }
   });
 });
