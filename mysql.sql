ba5c86e0e45b60240193a4e9d642b8f549c48056
marjoven <81245995@qq.com>
2018-07-20 18:36:38 +0800

    fix

--- mysql.sql
+++ mysql.sql
@@ -1,10 +1,23 @@
-create table users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    accout VARCHAR(20) UNIQUE NOT NULL ,
-    password VARCHAR(20) NOT NULL
+-- create table users (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     accout VARCHAR(20) UNIQUE NOT NULL ,
+--     password VARCHAR(20) NOT NULL
 
+
+-- );
+
+
+
+create table gameList (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) UNIQUE NOT NULL ,
+    releaseDate DATETIME
+
+    
 
 );
+
+
 
 /*
 source /Users/reborn/programs/express_mysql_es6/mysql.sql
@@ -13,3 +26,5 @@ insert into users (accout,password) values (1,2)
 
 select * from users;
 */
+
+
