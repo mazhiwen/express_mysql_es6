ba316ec8ba6ab6e8a1fa7643952d54dfa8eb4994
marjoven <81245995@qq.com>
2018-07-26 17:34:26 +0800

    cors配置

--- mysql.sql
+++ mysql.sql
@@ -8,14 +8,12 @@
 
 
 
-create table gameList (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) UNIQUE NOT NULL ,
-    releaseDate DATETIME
-
-    
-
-);
+-- create table gameList (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(50) UNIQUE NOT NULL ,
+--     developer VARCHAR(50),
+--     releaseDate DATETIME
+-- );
 
 
 
@@ -23,8 +21,11 @@ create table gameList (
 source /Users/reborn/programs/express_mysql_es6/mysql.sql
 alter table users AUTO_INCREMENT=10000;
 insert into users (accout,password) values (1,2)
+insert into gameList values (NULL,'22222','wwww','2018-04-05 05:12:12');
 
 select * from users;
 */
+
+-- 1532594205750
 
 
--- routes/game.js
+++ routes/game.js
@@ -19,24 +19,25 @@ router.get('/list', function(req, res, next) {
 });
 
 
-router.post('/create', function(req, res, next) {
-  // mysql.query(`INSERT INTO users (accout,password) values (${req.body.account},${req.body.password})`, function(err, rows, fields) {
-  //   if (err) {
-  //     let  message='错误';
-  //     if(err.code=='ER_DUP_ENTRY'){
-  //       message='用户名称重复'
-  //     }
-  //     res.send({
-  //       status:500,
-  //       message:message
-  //     });
-  //   }else{
-  //     res.send({
-  //       status:200,
-  //       message:'成功'
-  //     });
-  //   }
-  // });
+router.put('/create', function(req, res, next) {
+  let {name,developer,releaseDate}=req.body;
+  mysql.query(`INSERT INTO gameList values (NULL,${name},${developer},${releaseDate})`, function(err, rows, fields) {
+    if (err) {
+      let  message='错误';
+      if(err.code=='ER_DUP_ENTRY'){
+        message='用户名称重复'
+      }
+      res.send({
+        status:500,
+        message:message
+      });
+    }else{
+      res.send({
+        status:200,
+        message:'成功'
+      });
+    }
+  });
 });
 
 router.post('/login', function(req, res, next) {
